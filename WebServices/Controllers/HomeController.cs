6cde722911aff16a3cd63fe375281dc27b6d7b5d
Maksym Havryliuk <maks.havryliuk@gmail.com>
2019-05-23 17:23:23 +0300

    Added Controller add Views.

---
+++ WebServices/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ReservationRepository repo = ReservationRepository.Current;
+
+        public ViewResult Index()
+        {
+            return View(repo.GetAll());
+        }
+
+        public ActionResult Add(Reservation item)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Add(item);
+
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
+        }
+
+        public ActionResult Remove(int id)
+        {
+            repo.Remove(id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(Reservation item)
+        {
+            if (ModelState.IsValid && repo.Update(item))
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View("Index");
+        }
+    }
+}
