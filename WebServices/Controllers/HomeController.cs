282346d01e3da9071ceac620e15792f6ac735dbf
Maksym Havryliuk <maks.havryliuk@gmail.com>
2019-05-24 00:02:47 +0300

    Refactored code.

--- WebServices/Controllers/HomeController.cs
+++ WebServices/Controllers/HomeController.cs
@@ -1,44 +1,12 @@
 ﻿using System.Web.Mvc;
-using WebServices.Models;
 
 namespace WebServices.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ReservationRepository _repo = ReservationRepository.Current;
-
         public ViewResult Index()
         {
-            return View(_repo.GetAll());
-        }
-
-        public ActionResult Add(Reservation item)
-        {
-            if (ModelState.IsValid)
-            {
-                _repo.Add(item);
-
-                return RedirectToAction("Index");
-            }
-
-            return View("Index");
-        }
-
-        public ActionResult Remove(int id)
-        {
-            _repo.Remove(id);
-
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Update(Reservation item)
-        {
-            if (ModelState.IsValid && _repo.Update(item))
-            {
-                return RedirectToAction("Index");
-            }
-
-            return View("Index");
+            return View();
         }
     }
 }
